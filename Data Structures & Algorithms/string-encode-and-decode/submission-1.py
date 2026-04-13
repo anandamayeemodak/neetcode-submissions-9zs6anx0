a63a3daa7c8d6ddffb92defd5759db7d1240069b
anandamayeemodak <56431596+anandamayeemodak@users.noreply.github.com>
2026-04-12 22:47:36 -0400

    Add: string-encode-and-decode - submission-1

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-1.py
@@ -0,0 +1,13 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        res = ""
+        for s in strs:
+            res = res + "!_!" + s
+        
+        return res
+
+
+    def decode(self, s: str) -> List[str]:
+        res = s.split("!_!")
+        return res[1:]
