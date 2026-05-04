e307f01e36ff7243999c349061cf9152b1ca0846
anandamayeemodak <56431596+anandamayeemodak@users.noreply.github.com>
2026-05-04 00:45:52 -0500

    Add: partition-labels - submission-0

---
+++ Data Structures & Algorithms/partition-labels/submission-0.py
@@ -0,0 +1,35 @@
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        res = []
+        last_pos_map = {}
+
+        for i in range(len(s)):
+            char = s[i]
+            last_pos_map[char] = i
+
+        i = 0
+        while i<len(s):
+            char = s[i]
+            print("char: ", char)
+            subs_len = last_pos_map[char] - i + 1
+            print(subs_len)
+
+            j = 1
+            while j < subs_len:
+                print("j: ", j)
+                subs_char = s[i+j]
+                subs_len = max(last_pos_map[subs_char] - i + 1, subs_len)
+                print(subs_len)
+                j += 1
+            
+            res.append(subs_len)
+            i = subs_len + i
+        
+        return res
+
+
+
+        
+
+                
+        
